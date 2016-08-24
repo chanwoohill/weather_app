66aaeec710bd9c0c1e5d9531026e84ada3f49422
Chanwoo Kim <chanwoohill@gmail.com>
2016-08-24 22:54:00 +0000

    add jsonp requests for forecasts

--- application.js
+++ application.js
@@ -1,31 +1,57 @@
 $(function() {
 
 $('#cities').click(function() {
-  $("ul").empty();
+  $("#cityData").empty();
   var text =  $("#cityInput").val();
   var url = 'http://autocomplete.wunderground.com/aq?query=' + text + '&cb=?';
-  var key = "e401293040a26d38"; 
   console.log(url);
+
+//Example resposne API call http://api.wunderground.com/api/d99eb29a20700397/forecast/q/zmw:94125.1.99999.json
 
   $.ajax({
     dataType: "json",
     url: url,
     success: function(data) {
       var cities = data.RESULTS;
-      for(var i=0; i < cities.length; i++) {
-        var city = cities[i];  
-        $("ul").append("<li>" + city.name + "</li>");  
+      console.log(cities)
+      for(var i = 0; i < cities.length; i++) {
+        var city = cities[i]; 
+        var key = "d99eb29a20700397";  
+        $("#cityData").append("<li data-url='" + "http://api.wunderground.com/api/" + key + "/forecast" + city.l + ".json'>" + city.name + "</li>");  ;  
       }
-
     }
+  });
 
+  $("body").on('click', '[data-url]', function(){
+    var url = $(this).attr("data-url");
+      $.ajax({
+        url: url,
+        dataType: "jsonp",
+        success: function(data) {
+          console.log(data);
+           $('.weatherReport').empty();
+           $('.icon').empty();
+          $(".weatherReport").append("<p>" + data.forecast.txt_forecast.forecastday[0].fcttext + "</p>");
+
+          $(".icon").append("<img src=" + data.forecast.txt_forecast.forecastday[0].icon_url + ">");
+        }
+      })
   });
 
 
 
-
-
 });
 
 
 });
+
+
+
+
+
+
+
+
+
+
+
